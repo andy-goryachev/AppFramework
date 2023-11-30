f7da4b5f91ddb2be0f0ad1246b0e87f744bbe66a
andy goryachev <andy@goryachev.com>
2023-11-29 21:28:43 -0800

    list view

---
+++ src/goryachev/demo/Entry.java
@@ -0,0 +1,17 @@
+// Copyright © 2023 Andy Goryachev <andy@goryachev.com>
+package goryachev.demo;
+
+
+/**
+ * Entry.
+ */
+public class Entry
+{
+	public final String title;
+	
+	
+	public Entry(String title)
+	{
+		this.title = title;
+	}
+}
--- src/goryachev/demo/MainWindow.java
+++ src/goryachev/demo/MainWindow.java
@@ -1,6 +1,22 @@
 // Copyright © 2023 Andy Goryachev <andy@goryachev.com>
 package goryachev.demo;
+import goryachev.fx.FxMenuBar;
+import goryachev.fx.FxTabPane;
+import goryachev.fx.FxToolBar;
 import goryachev.fx.FxWindow;
+import goryachev.fx.icon.StarIcon;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Side;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 
 /**
@@ -8,10 +24,90 @@ import goryachev.fx.FxWindow;
  */
 public class MainWindow extends FxWindow
 {
+	protected final TextField searchField;
+	protected final DemoListWithPreviewPane listView;
+	protected final ObservableList<Entry> items = FXCollections.observableArrayList();
+	
+	
 	public MainWindow()
 	{
 		super("MainWindow");
 		setTitle("App Framework Demo Window");
 		setSize(1150, 800);
+		
+		searchField = new TextField();
+		searchField.setPrefColumnCount(20);
+		
+		items.setAll
+		(
+			new Entry("1"),
+			new Entry("2"),
+			new Entry("3")
+		);
+		
+		listView = new DemoListWithPreviewPane(items);
+		
+		FxTabPane sidePane = new FxTabPane();
+		sidePane.setSide(Side.LEFT);
+		int sz = 20;
+		sidePane.addTab(t(new StarIcon(sz, Color.BLACK), "List With Preview", listView));
+		sidePane.addTab(t(new StarIcon(sz, Color.GRAY), null, null));
+		sidePane.addTab(t(new StarIcon(sz, Color.GRAY), null, null));
+		sidePane.addTab(t(new StarIcon(sz, Color.GRAY), null, null));
+		sidePane.addTab(t(new StarIcon(sz, Color.GRAY), null, null));
+		
+		VBox vb = new VBox
+		(
+			createMenu(),
+			createToolBar()
+		);
+		
+		setTop(vb);
+		setCenter(sidePane);
+		setBottom(createStatusBar());
+	}
+
+	
+	private FxMenuBar createMenu()
+	{
+		FxMenuBar m = new FxMenuBar();
+		m.menu("File");
+		m.menu("Edit");
+		m.menu("Window");
+		m.menu("Help");
+		return m;
+	}
+	
+	
+	private FxToolBar createToolBar()
+	{
+		FxToolBar t = new FxToolBar();
+		t.add(new Button("#1"));
+		t.add(new Button("#2"));
+		t.add(new Button("#3"));
+		t.add(new Button("#4"));
+		t.fill();
+		t.add(new Label("Find: "));
+		t.add(searchField);
+		return t;
+	}
+	
+	
+	private Node createStatusBar()
+	{
+		BorderPane p = new BorderPane();
+		p.setRight(new Label("copyright © 2023 andy goryachev  "));
+		return p;
+	}
+	
+	
+	private Tab t(Node icon, String tooltip, Node p)
+	{
+		Tab t = new Tab();
+		t.setGraphic(icon);
+		t.setClosable(false);
+		t.setContent(p);
+		t.setTooltip(new Tooltip(tooltip));
+		return t;
 	}
 }
